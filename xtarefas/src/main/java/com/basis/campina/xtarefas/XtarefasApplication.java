e87703eed0e1b3dd207085995787839332c2c939
luan.alencar <luan.alencar@basis.com.br>
2021-07-19 17:25:11 -0300

    primeiro commit

---
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/XtarefasApplication.java
@@ -0,0 +1,13 @@
+package com.basis.campina.xtarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtarefasApplication.class, args);
+	}
+
+}
