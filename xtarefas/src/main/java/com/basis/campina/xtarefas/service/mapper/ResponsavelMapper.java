bcbf087afc001469701064dab5837d05097598bb
luan-alencar <luanalencar134@gmail.com>
2021-07-20 07:57:14 -0300

    Started XtarefasApplication

--- xtarefas/src/main/java/com/basis/campina/xtarefas/service/mapper/ResponsavelMapper.java
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/service/mapper/ResponsavelMapper.java
@@ -3,7 +3,22 @@ package com.basis.campina.xtarefas.service.mapper;
 
 import com.basis.campina.xtarefas.domain.Responsavel;
 import com.basis.campina.xtarefas.service.dto.ResponsavelDTO;
+import org.mapstruct.Mapper;
 
+import java.util.List;
 
+@Mapper(componentModel = "spring", uses = {})
 public interface ResponsavelMapper extends EntityMapper<ResponsavelDTO, Responsavel> {
+
+    @Override
+    Responsavel toEntity(ResponsavelDTO dto);
+
+    @Override
+    ResponsavelDTO toDTO(Responsavel entity);
+
+    @Override
+    List<Responsavel> toEntity(List<ResponsavelDTO> dtoList);
+
+    @Override
+    List<ResponsavelDTO> toDTO(List<Responsavel> entityList);
 }
